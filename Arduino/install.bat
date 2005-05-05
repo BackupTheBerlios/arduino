85ea6f25b7839c91c6a902e580c04541c869732c
mellis <mellis>
2005-05-05 14:04:46 +0000

    Fixed typo, added delete of tmp/dep/readme.txt

--- Arduino/install.bat
+++ Arduino/install.bat
@@ -8,10 +8,11 @@ IF %WIRINGDIR%=="" GOTO ERROR
 
 COPY wiringlite\makefile.win wiringlite\makefile
 COPY wiringlite\include\makefile.win wiringlite\include\makefile
-COPY wiringlite\bin\gnumake.win wiringlite\bin\gnumake,exe
+COPY wiringlite\bin\gnumake.win wiringlite\bin\gnumake.exe
 COPY pde.jar %WIRINGDIR%\lib
 MKDIR %WIRINGDIR%\lib\wiringlite
 XCOPY /y /s wiringlite %WIRINGDIR%\lib\wiringlite
+DEL %WIRINGDIR%\lib\wiringlite\tmp\dep\README.txt
 
 GOTO END
 
